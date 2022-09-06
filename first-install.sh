1d52b2d8abb3ac0c8e44bafd1588a38a184111d6
Vineet Kulkarni <kvineet@localhost.localdomain>
2022-09-06 23:02:04 +0530

    fixed first install

--- first-install.sh
+++ first-install.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env sh
 
+if command -v pacman &> /dev/null
+then
+    printf "pacman detected. Assuming Arch based system."
+    sudo pacman -S which git zsh --noconfirm
+elif command -v apt &> /dev/null
+then
+    printf "apt detected. Assuming Ubuntu based system."
+    sudo apt install -y which git zsh
+elif command -v dnf &> /dev/null
+then
+     printf "dnf detected. Assuming Fedora."
+     sudo dnf install -y which git zsh
+else
+    printf "No known package manager detected. Ensure which, git and zsh are installed"
+fi
+
 BINDIR=$HOME/.local/bin sh -c "$(curl -fsLS git.io/chezmoi)"
 echo "PATH=\$PATH:$HOME/.local/bin" >> $HOME/.profile
 
 . $HOME/.profile
 
 chezmoi init
-chezmoi apply
-
+chezmoi apply --no-tty
