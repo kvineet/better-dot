5ed5cbfc777e14ad98d7d73d80c2c1c2a211ca8e
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2023-12-14 20:31:55 +0530

    added documentation

---
+++ first-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+sudo mkdir /etc/nix
+# write nix.conf
+sudo /bin/cat <<EOF > /etc/nix/nix.conf
+build-users-group = nixbld
+extra-experimental-features = nix-command flakes
+use-xdg-base-directories = true
+EOF
+
+# install nix
+bash <(curl -L https://nixos.org/nix/install) --daemon
+
+echo ". $HOME/.nix-profile/etc/profile.d/nix.sh" >> ~/.profile
+. .profile
+
+# add home manager
+nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
