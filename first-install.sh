f301b87bed7d8ff71984e940b5a4094b97c3b08b
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2023-12-24 04:37:10 +0530

    improved first-install.sh

--- first-install.sh
+++ first-install.sh
@@ -6,14 +6,16 @@ export XDG_DATA_HOME=$HOME/.local/share
 export NIX_PATH=$XDG_STATE_HOME/nix/defexpr/channels
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
-git clone git@kvineet.github.com:kvineet/better-dot.git
-
 DOT_DIR=~/projects/better-dot
+git clone https://github.com/kvineet/better-dot.git $DOT_DIR
+
+echo "machine name"
+read machine
 nix run $DOT_DIR switch \
     --extra-experimental-features nix-command \
     --extra-experimental-features flakes \
     -- \
-    --flake $DOT_DIR.#msft \
+    --flake $DOT_DIR.#$machine \
     switch \
     --extra-experimental-features nix-command \
     --extra-experimental-features flakes
