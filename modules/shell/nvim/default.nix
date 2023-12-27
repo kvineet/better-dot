bb68ecdb3b8c573dafb447baa6751e429da352d7
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2023-12-27 17:53:35 +0530

    fixed default editor

--- modules/shell/nvim/default.nix
+++ modules/shell/nvim/default.nix
@@ -1,10 +1,12 @@
 {...}:{
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
-
-  xdg.configFile.nvim.source = builtins.fetchGit {
-    url = "https://github.com/doom-neovim/doom-nvim.git";
-    ref = "main";
-    rev = "70ceebbbe74f5399d52640c4c7cadb1b4a6b2844";
-  }; 
+  # xdg.configFile.nvim = {
+  #   source = builtins.fetchGit {
+  #     url = "https://github.com/doom-neovim/doom-nvim.git";
+  #     ref = "main";
+  #     rev = "70ceebbbe74f5399d52640c4c7cadb1b4a6b2844";
+  #   };
+  #   recursive = true;
+  # }; 
 }
