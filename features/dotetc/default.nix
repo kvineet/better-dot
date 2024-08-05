d0e4dc73ef4c40b90d44e28f23577400dce31235
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2024-08-05 13:35:34 +0530

    added chezmoi activation

--- features/dotetc/default.nix
+++ features/dotetc/default.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config, ... }: {
   home = {
     packages = with pkgs;
       [
         # ...various
         chezmoi
       ];
+    # TODO: bootstrap dot files
+    activation.chezmoi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    DRY_RUN_ARG=""
+    if ! [ -z $DRY_RUN_CMD ]; then
+       DRY_RUN_ARG="--dry-run"
+    fi
+    ${lib.getExe pkgs.chezmoi} $VERBOSE_ARG $DRY_RUN_ARG init --apply
+    '';
   };
-  # TODO: bootstrap dotfiles
 }
