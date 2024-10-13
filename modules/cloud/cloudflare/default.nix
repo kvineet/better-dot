6c9bba2c69a10e84356636b81c37c78ff13130fc
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2024-10-13 20:53:56 +0530

    cloudflare tunnel config

--- modules/cloud/cloudflare/default.nix
+++ modules/cloud/cloudflare/default.nix
@@ -1,5 +1,12 @@
-{ pkgs, mypkgs, ... }: {
+{ pkgs, mypkgs, ... }:
+{
   home.packages = [
     pkgs.cloudflared
   ];
+  programs.ssh.extraConfig = ''
+    Match host ssh.kvineet.com exec "${pkgs.cloudflared}/bin/cloudflared access ssh-gen --hostname %h"
+      ProxyCommand ${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h
+      IdentityFile ~/.cloudflared/%h-cf_key
+      CertificateFile ~/.cloudflared/%h-cf_key-cert.pub
+  '';
 }
